1b375bd4ac5ff4aa88bcc44dc37babef268422c4
thauaaf <thaua.fellype18@gmail.com>
2024-09-05 00:19:54 -0300

    Add project files.

---
+++ StringCount/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StringCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra");
+            string palavra = Console.ReadLine();
+
+            char letraMin = 'a', letraMa = 'A';
+           
+            int count = 0;
+
+            foreach (char caractere in palavra)
+            {
+                if (caractere == letraMin || caractere == letraMa)
+                {
+                    count++;
+                } 
+
+            }
+
+            if (count == 0) { Console.WriteLine("A palavra não possui a letra 'A'. "); }
+
+            else
+            {
+                Console.WriteLine($"A letra 'A' aparece {count} vezes.");
+            }
+        }
+    }
+}
